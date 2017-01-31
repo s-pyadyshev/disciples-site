139b3c08f9cba74f4b3893c31ddf4c887c1813e2
s.pyadyshev <s.pyadyshev@mail.ru>
2017-02-01 01:03:43 +0300

    Fixes script, adds some src files

--- js/internal.js
+++ js/internal.js
@@ -1,37 +1,25 @@
-// http://stackoverflow.com/questions/18307078/jquery-best-practises-in-case-of-document-ready
-
 $(document).ready(function() {
 
     function buildTown(town) {
         var town = town;
 
         $(".b-build").on("click", function(){
+            var $this = $(this),
+                $town = $(town);
 
-            if ($(this).hasClass("is-active") === false) {
-                var btnClass = $(this).attr("class").substr(8);
-                var imgClass = $(town).find("." + btnClass);
+            if ($this.hasClass("is-active") === false) {
+                var btnClass = $this.attr("class").substr(8);
+                var imgClass = $town.find("." + btnClass);
 
-                $(this).addClass("is-active");
+                $this.addClass("is-active");
                 imgClass.show();
-            } else if ($(this).hasClass("is-active") === true) {
-                var btnClass = $(this).removeClass("is-active").attr("class").substr(8);
-                var imgClass = $(town).find("." + btnClass);
+            } else if ($this.hasClass("is-active") === true) {
+                var btnClass = $this.removeClass("is-active").attr("class").substr(8);
+                var imgClass = $town.find("." + btnClass);
 
-                $(this).removeClass("is-active");
+                $this.removeClass("is-active");
                 imgClass.hide();
             }
         });
     }
-
-    buildTown(".b-empire");
-
-    // СКРИПТЫ, СРАБАТЫВАЮЩИЕ ПОСЛЕ ПОЛНОЙ ЗАГРУЗКИ СТРАНИЦЫ (фреймы, объекты, картинки)
-    $(window).on("load", function () {
-
-    });
-
-    // СКРИПТЫ, СРАБАТЫВАЮЩИЕ ПРИ ИЗМЕНЕНИИ ОКНА ПРОСМОТРА
-    $(window).on("resize", function () {
-
-    });
 });
