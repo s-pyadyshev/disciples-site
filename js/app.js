3307ecc9d6a21d501ad6cce8d3a60c6a8c319bbf
s.pyadyshev <s.pyadyshev@mail.ru>
2017-03-01 23:37:00 +0800

    Adds js build town script

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
 function buildTown(town) {
     var town = document.querySelector(town);
-    var buildBtn = document.querySelectorAll('.b-build');
+    var buildBtns = document.querySelectorAll('.b-build');
+    var buildBtnArray = [];
 
-    for (var i = 0; i < buildBtn.length; i++) {
-        buildBtn[i].addEventListener('click', function() {
+    for (var i = buildBtns.length; i--; buildBtnArray.unshift(buildBtns[i]));
+
+    for (var i = 0; i < buildBtnArray.length; i++) {
+
+        buildBtnArray[i].addEventListener('click', function() {
             var self = this;
 
             if (self.classList.contains('is-active') === false) {
                 var btnClass = self.getAttribute('class').substr(8);
-                var imgClass = self.querySelector('.' + btnClass);
-
+                var imgClass = town.querySelector('.' + btnClass);
+ 
                 self.classList.add('is-active');
                 imgClass.style.display = 'block';
 
             } else if (self.classList.contains('is-active') === true) {
-                var btnClass = self.classList.remove('is-active').getAttribute('class').substr(8);
+                var btnClass = self.getAttribute('class').substr(8).slice(0, -10);
                 var imgClass = town.querySelector('.' + btnClass);
 
                 self.classList.remove('is-active');
--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,31 @@
 function buildTown(town) {
     var town = document.querySelector(town);
-    var buildBtn = document.querySelectorAll('.b-build');
+    var buildBtns = document.querySelectorAll('.b-build');
+    var buildBtnArray = [];
 
-    buildBtn.addEventListener('click', function() {
-        var self = $(this);
+    for (var i = buildBtns.length; i--; buildBtnArray.unshift(buildBtns[i]));
 
-        if (self.classList.contains('is-active') === false) {
-            var btnClass = self.getAttribute('class').substr(8);
-            var imgClass = self.find("." + btnClass);
+    for (var i = 0; i < buildBtnArray.length; i++) {
 
-            self.classList.add('is-active');
-            imgClass.style.display = 'block';
-        } else if (self.classList.contains('is-active') === true) {
-            var btnClass = self.classList.remove('is-active').getAttribute('class').substr(8);
-            var imgClass = town.querySelectorAll('.' + btnClass);
-        }
+        buildBtnArray[i].addEventListener('click', function() {
+            var self = this;
 
-    });
+            if (self.classList.contains('is-active') === false) {
+                var btnClass = self.getAttribute('class').substr(8);
+                var imgClass = town.querySelector('.' + btnClass);
+ 
+                self.classList.add('is-active');
+                imgClass.style.display = 'block';
+
+            } else if (self.classList.contains('is-active') === true) {
+                var btnClass = self.getAttribute('class').substr(8).slice(0, -10);
+                var imgClass = town.querySelector('.' + btnClass);
+
+                self.classList.remove('is-active');
+                imgClass.style.display = 'none';
+            }
+        });
+    }
 }
 
 buildTown('.b-empire');
